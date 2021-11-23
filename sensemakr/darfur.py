90be985a501fc91122fca9540e4fe5ea702e0c88
KennyZhang-17 <zhehao_zhang@umail.ucsb.edu>
2021-11-23 12:31:19 -0800

    Try new read file

--- sensemakr/darfur.py
+++ sensemakr/darfur.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import os
+
+path=os.path.join(os.path.dirname(__file__), '../data/darfur.csv')
 
 def load_darfur():
-    return pd.read_csv('data/darfur.csv')
+    return pd.read_csv(path)
