6e811f9bb6b68a35088218d067e60c7d048f7a5f
Carlos Cinelli <carloscinelli@hotmail.com>
2021-12-30 23:54:57 -0800

    globaltoc

--- sensemakr/docs/conf.py
+++ sensemakr/docs/conf.py
@@ -107,9 +107,9 @@ html_theme_options = {
 
 }
 
-# html_sidebars = {
-#     "**": [],
-# }
+html_sidebars = {
+    '**': ['globaltoc.html'],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
