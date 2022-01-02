6c207520e157ebdbcec88c4b0ac4273cd071653d
Carlos Cinelli <carloscinelli@hotmail.com>
2022-01-01 19:00:06 -0800

    init load_darfur

--- sensemakr/__init__.py
+++ sensemakr/__init__.py
@@ -7,3 +7,4 @@ from sensemakr.ovb_plots import ovb_contour_plot, ovb_extreme_plot, add_bound_to
 from sensemakr.bias_functions import adjusted_estimate, adjusted_t, adjusted_se, adjusted_partial_r2, bias, relative_bias, rel_bias
 from sensemakr.ovb_bounds import ovb_bounds, ovb_partial_r2_bound
 from sensemakr.sensitivity_stats import robustness_value, partial_r2, partial_f2, partial_f, group_partial_r2
+from sensemakr.data import load_darfur
